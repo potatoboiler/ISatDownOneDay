bcc1e99cec2b219d41dabfad98197d9d1602135d
potatoboiler <1nova2hunter3@gmail.com>
2021-07-13 09:16:46 -0400

    Implement base solver class

---
+++ src/solver.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <algorithm>
+
+#include "solver.hpp"
+
+template<typename ... lits> requires all_literal<lits...>
+void Solver::add_clause(lits ... literals) {
+	this->cnf.emplace_back(literals...);
+}
+void Solver::add_clause(clause in) {
+	this->cnf.push_back(in);
+}
+void Solver::add_clause(clause&& in) {
+	this->cnf.push_back(in);
+}
+void Solver::add_clause(clause const & in) {
+	this->cnf.push_back(in);
+}
+
+size_t Solver::count_vars() {
+	// if literal type changes, this also needs to change
+	size_t out = 0;
+	for (auto & cl : this->cnf) {
+		for (auto lit : cl) {
+			out = std::max( (size_t)std::abs(lit), out );
+		}
+	}
+	return out;
+}
---
+++ src/solver.hpp
@@ -0,0 +1,40 @@
+#ifndef SOLVER_BASE
+#define SOLVER_BASE
+#include <vector>
+
+#include "DIMACS.hpp"
+
+using literal = int;
+using clause = std::vector<literal>;
+using cnf_t = std::vector<clause>;
+
+template<typename ... Ts>
+concept all_literal = std::same_as<literal, std::common_type<Ts...>>;
+
+
+class Solver {
+	// remember to inherit virtually from this class
+	public:
+		Solver() : cnf(), n_vars(0), n_clauses(0) {};
+		Solver(cnf_t const & input) : cnf(input), n_clauses(input.size()) { this->n_vars = count_vars(); };
+		Solver(cnf_t&& input) : cnf(input), n_clauses(input.size()) { this->n_vars = count_vars(); };
+
+		virtual void solve() = 0;
+
+		template<typename ... lits> requires all_literal<lits...>
+		void add_clause(lits ...);
+		void add_clause(clause);
+		void add_clause(clause&&);
+		void add_clause(clause const &);
+		
+		void change_cnf(cnf_t);
+		void change_cnf(cnf_t&&);
+		void change_cnf(cnf_t const &);
+
+		size_t count_vars();
+	protected:
+		cnf_t cnf;
+		size_t n_vars;
+		size_t n_clauses;
+};
+#endif
