9c72a7e5c9c7e33cdb0de585c6ac84fb1fccc93e
potatoboiler <potatoboiler@github.com>
2021-11-08 14:39:12 -0800

    Format and privatize the virtual class

--- src/base/solver.hpp
+++ src/base/solver.hpp
@@ -7,37 +7,40 @@ using literal = int;
 using clause = std::vector<literal>;
 using cnf_t = std::vector<clause>;
 
-template<typename ... Ts>
+template <typename... Ts>
 concept all_literal = std::same_as<literal, std::common_type<Ts...>>;
 
-
-class Solver {
+class Solver
+{
 	// remember to inherit virtually from this class
-	public:
-		Solver() : cnf(), n_vars(0), n_clauses(0), model(n_vars) {};
-		Solver(cnf_t const & input) : cnf(input), n_clauses(input.size()), n_vars(count_vars()), model(n_vars) {};
-		Solver(cnf_t&& input) : cnf(input), n_clauses(input.size()), n_vars(count_vars()), model(n_vars) {};
+public:
+	Solver() : cnf(), n_vars(0), n_clauses(0), model(n_vars){};
+	Solver(cnf_t const &input) : cnf(input), n_clauses(input.size()), n_vars(count_vars()), model(n_vars){};
+	Solver(cnf_t &&input) : cnf(input), n_clauses(input.size()), n_vars(count_vars()), model(n_vars){};
 
-		virtual bool solve() = 0;
+	template <typename... lits>
+	requires all_literal<lits...>
+	void add_clause(lits...);
+	void add_clause(clause);
+	void add_clause(clause &&);
+	void add_clause(clause const &);
 
-		template<typename ... lits> requires all_literal<lits...>
-		void add_clause(lits ...);
-		void add_clause(clause);
-		void add_clause(clause&&);
-		void add_clause(clause const &);
-		
-		void change_cnf(cnf_t);
-		void change_cnf(cnf_t&&);
-		void change_cnf(cnf_t const &);
+	void change_cnf(cnf_t);
+	void change_cnf(cnf_t &&);
+	void change_cnf(cnf_t const &);
 
-		//
-		bool unit_propagate();
+	//
+	bool unit_propagate();
 
-		size_t count_vars();
+	size_t count_vars();
 
-	protected:
-		cnf_t cnf;
-		std::vector<literal> model;
-		size_t n_vars;
-		size_t n_clauses;
+protected:
+	cnf_t cnf;
+	std::vector<literal> model;
+	size_t n_vars;
+	size_t n_clauses;
+
+private:
+	virtual ~Solver() = default;
+	virtual bool solve() = 0;
 };
